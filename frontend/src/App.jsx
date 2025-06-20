27bb613acd09be0c956b8f579b8d4deef92bcf46
AlejandroPelcas <alejandro.pelcas@berkeley.edu>
2025-06-19 21:06:53 -0700

    Added profile pic and about section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,8 +18,6 @@ function App() {
       </div>
       <br></br>
       <div>
-        Socials:
-        <br></br>
         <MyButton title="LinkedIn" link="https://www.linkedin.com/in/alejandro-pelcastre/"> </MyButton>
     
       </div>
@@ -28,13 +26,25 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* About me and profile photo */}
+      <div className="flex items-center space-x-4 p-4 bg-white rounded-lg shadow">
+        <img
+          src="https://media.licdn.com/dms/image/v2/D5603AQHZLbdwyxFrwg/profile-displayphoto-shrink_800_800/B56ZTykKSuHoAg-/0/1739236359457?e=1755734400&v=beta&t=Hg--frPTbW5qU3CJlNApaxl2mNfBnra6PlhcVTN81qc"
+          alt="Profile"
+          className="w-100 h-100 rounded-full object-cover"
+        />
+        
+        <div>
+          <h2 className="text-lg font-semibold text-gray-900">Alejandro Pelcastre-Ramirez</h2>
+          <p className="text-sm text-gray-600">
+          Hi! I'm Alejandro. I'm an educator and life learner specializing in tutoring introductory Physics, Data Sciece, and Calculus.
+          I graduated from UC Berkeley with a B.A. in Applied Mathematics and Physics and a
+          Masters in Data Science. I've been tutoring and teaching math and science for nearly 10 years now as well as 
+          teaching chess, working as a preK teacher, and graduate student instructor at UC Berkeley.           </p>
+        </div>
+      </div>
     </>
   )
 }
